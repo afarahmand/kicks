ebce62c30630d4fc2a8354f33399f3576646e3f7
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-01-31 13:14:52 -0800

    Complete back end auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,39 @@
 class User < ApplicationRecord
+  validates :email, :name, :password_digest, :session_token, presence: true
+  validates :password, length: { minimum: 4, allow_nil: true }
+  validates :name, length: { minimum: 1 }
+  validates :email, uniqueness: true
+
+  before_validation :ensure_session_token
+
+  attr_reader :password
+
+  def password=(pw)
+    @password = pw
+    self.password_digest = BCrypt::Password.create(pw)
+  end
+
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def self.find_by_credential(email, pw)
+    user = User.find_by(email: email)
+
+    if user && user.is_password?(pw)
+      return user
+    else
+      return nil
+    end
+  end
 end
