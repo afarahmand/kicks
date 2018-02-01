7b25ddcbc6cccf181b71a667d28c97046986edbc
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-01-31 16:20:56 -0800

    Complete session api util

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 // import Root from './components/root';
 // import configureStore from './store/store';
 
+// import { signin, signout, signup }
+//   from './utils/session_api_util';
+
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
   // const store = configureStore();
 
+  // window.signin = signin;
+  // window.signout = signout;
+  // window.signup = signup;
+
   //ReactDOM.render(<Root store={store}/>, root);
-  ReactDOM.render(<h1> ...React is working... </h1>, root);
+  ReactDOM.render(<h1> React is working... </h1>, root);
 });
---
+++ frontend/utils/session_api_util.js
@@ -0,0 +1,22 @@
+export const signin = user => (
+  $.ajax({
+    url: '/api/session',
+    method: 'POST',
+    data: { user }
+  })
+);
+
+export const signout = () => (
+  $.ajax({
+    url: '/api/session',
+    method: 'DELETE'
+  })
+);
+
+export const signup = user => (
+  $.ajax({
+    url: '/api/users',
+    method: 'POST',
+    data: { user }
+  })
+);
