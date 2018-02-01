cd70beee95a81fcd8953ae4631cbf9ae4c46e9e0
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-01-31 18:04:06 -0800

    Entry file creates store

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import Root from './components/root';
-// import configureStore from './store/store';
+import configureStore from './store/store';
 
 // import { signin, signout, signup }
 //   from './utils/session_api_util';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
-  // const store = configureStore();
+  const store = configureStore();
 
   // window.signin = signin;
   // window.signout = signout;
   // window.signup = signup;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
 
   //ReactDOM.render(<Root store={store}/>, root);
   ReactDOM.render(<h1> React is working... </h1>, root);
