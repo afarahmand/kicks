7fb5e864bc9ef7071d358277d3efad287b117a32
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-02-03 22:44:51 -0800

    Add api util file for projects and testing code in entry file

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -6,6 +6,14 @@ import configureStore from './store/store';
 import { signin, signout, signup }
   from './actions/session_actions';
 
+import {
+  fetchProjects,
+  fetchProject,
+  createProject,
+  updateProject,
+  deleteProject
+} from './utils/project_api_util';
+
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
   let store;
@@ -18,10 +26,26 @@ document.addEventListener("DOMContentLoaded", () => {
     store = configureStore();
   }
 
+  // window.fetchProjects = fetchProjects; // W
+  // window.fetchProject = fetchProject;   // W
+  // window.createProject = createProject; // W
+  // window.updateProject = updateProject; // W
+  // window.deleteProject = deleteProject; // W
+  // window.firstProject = {
+  //   category_id: 1,
+  //   description: "description",
+  //   funding_amount: 1,
+  //   funding_end_date: "2018-09-04T13:12:52.594Z",
+  //   id: 1,
+  //   image_url: "https://imgur.com/a/ZgBj0",
+  //   short_blurb: "blurb",
+  //   title: "Test",
+  //   user_id: 1
+  // };
   // window.signin = signin;
   // window.signout = signout;
   // window.signup = signup;
-  // window.getState = store.getState;
+  window.getState = store.getState;
   // window.dispatch = store.dispatch;
 
   ReactDOM.render(<Root store={ store } />, root);
---
+++ frontend/utils/project_api_util.js
@@ -0,0 +1,36 @@
+export const fetchProjects = () => (
+  $.ajax({
+    url: '/api/projects',
+    method: 'GET'
+  })
+);
+
+export const fetchProject = id => (
+  $.ajax({
+    url: `api/projects/${id}`,
+    method: 'GET'
+  })
+);
+
+export const createProject = project => (
+  $.ajax({
+    url: 'api/projects',
+    method: 'POST',
+    data: { project }
+  })
+);
+
+export const updateProject = project => (
+  $.ajax({
+    url: `api/projects/${project.id}`,
+    method: 'PATCH',
+    data: { project }
+  })
+);
+
+export const deleteProject = id => (
+  $.ajax({
+    url: `api/projects/${id}`,
+    method: 'DELETE'
+  })
+);
