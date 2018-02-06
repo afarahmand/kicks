b6f9abab370952c39b0f11725e663d010de65313
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-02-06 10:10:55 -0800

    Add project index and show containers with corresponding Routes

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -4,6 +4,8 @@ import { AuthRoute, ProtectedRoute } from '../utils/route_util';
 
 import GreetingContainer from './greeting/greeting_container';
 import ProjectFormContainer from './project/project_form_container';
+import ProjectIndexContainer from './project/project_index_container';
+import ProjectShowPageContainer from './project/project_show_page_container';
 import SessionFormContainer from './session/session_form_container';
 
 const App = (store) => (
@@ -17,8 +19,8 @@ const App = (store) => (
       <AuthRoute exact path="/signin" component={SessionFormContainer} />
       <AuthRoute exact path="/signup" component={SessionFormContainer} />
       <ProtectedRoute exact path="/projects/new" component={ProjectFormContainer} />
-      // this.props.history.push('/');
-      <Route path="/"/>
+      <Route path="/projects/:projectId" component={ProjectShowPageContainer} />
+      <Route path="/" component={ProjectIndexContainer} />
     </Switch>
   </div>
 );
