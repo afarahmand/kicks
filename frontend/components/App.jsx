c12e76d510a45ad0e5c2e8d7990f35c0d8c73e04
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-02-06 20:09:32 -0800

    Add a route that renders the home page component

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router';
 import { AuthRoute, ProtectedRoute } from '../utils/route_util';
 
-import GreetingContainer from './greeting/greeting_container';
+// import GreetingContainer from './greeting/greeting_container';
 import ProjectFormContainer from './project/project_form_container';
+import HomePage from './home/home_page';
 import ProjectIndexContainer from './project/project_index_container';
 import ProjectShowPageContainer from './project/project_show_page_container';
 import SessionFormContainer from './session/session_form_container';
+import NavbarContainer from './navbar/navbar_container';
 
 const App = (store) => (
   <div>
     <header>
-      <h1>Quikstarter: React is rendering App</h1>
-      <GreetingContainer/>
+      <NavbarContainer/>
     </header>
 
     <Switch>
@@ -20,7 +21,7 @@ const App = (store) => (
       <AuthRoute exact path="/signup" component={SessionFormContainer} />
       <ProtectedRoute exact path="/projects/new" component={ProjectFormContainer} />
       <Route path="/projects/:projectId" component={ProjectShowPageContainer} />
-      <Route path="/" component={ProjectIndexContainer} />
+      <Route path="/" component={HomePage} />
     </Switch>
   </div>
 );
