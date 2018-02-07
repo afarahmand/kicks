29762b7fdc68416fb1cde87aa83527aedccb09b0
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-02-06 20:07:19 -0800

    Created StatBar component for home page

---
+++ frontend/components/home/stat_bar.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+const StatBar = props => {
+  return (
+    <section className="stat-bar content-wide">
+      <ul className="content-narrow">
+        <li>
+          <div>
+            <span className="stat-bar-title">
+              Today's Date
+            </span>
+            <span className="stat-bar-body">
+              Bringing creative projects to life.
+            </span>
+          </div>
+        </li>
+        <li>
+          <div>
+            <span className="stat-bar-title">
+              Total Backers
+            </span>
+            <span className="stat-bar-body">
+              14,168,977
+            </span>
+          </div>
+        </li>
+        <li>
+          <div>
+            <span className="stat-bar-title">
+              Funded Projects
+            </span>
+            <span className="stat-bar-body">
+              138,577
+            </span>
+          </div>
+        </li>
+        <li>
+          <div>
+            <span className="stat-bar-title">
+              Live Projects
+            </span>
+            <span className="stat-bar-body">
+              3,843
+            </span>
+          </div>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default StatBar;
