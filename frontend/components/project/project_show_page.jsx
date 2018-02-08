98354288b0512ad5378da2a3a2ec7d52270fc661
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-02-08 13:03:52 -0800

    Styled project show page

--- frontend/components/project/project_show_page.jsx
+++ frontend/components/project/project_show_page.jsx
@@ -18,25 +18,64 @@ class ProjectShowPage extends React.Component {
 
   render() {
     if (!this.props.project) {
-      console.log("!!!");
-      console.log(this.props.project);
       return null;
     }
 
     return (
-      <div>
-        <span>THE PROJECT SHOW PAGE</span>
-        {this.props.project.title}
-        {this.props.project.image_url}
-        {this.props.project.description}
-        {this.props.project.short_blurb}
-        {this.props.project.funding_amount}
-        {this.props.project.funding_end_date}
-        {this.props.project.category}
+      <div className="project-show-page">
+
+        <section className="title content-narrow">
+          <div className="creator">
+          </div>
+          <div className="titles">
+            <h2>{this.props.project.title}</h2>
+            <span className="subtitle">{this.props.project.short_blurb}</span>
+          </div>
+        </section>
+
+        <section className="show-status content-narrow">
+          <img src={this.props.project.image_url}>
+          </img>
+
+          <div className="status">
+            <span className="one goal">${this.props.project.funding_amount}</span>
+            <span className="two">funding goal</span>
+            <span className="one">{this.props.project.funding_end_date}</span>
+            <span className="two">days to go</span>
+
+            <div className="all-nothing-container">
+              <span className="all-nothing">All or nothing.</span>
+              <span>
+                This project will only be funded if it reaches its goal
+                by {this.props.project.funding_end_date}.
+              </span>
+            </div>
+          </div>
+        </section>
+
+        <section className="description-rewards content-narrow">
+          <main className="description">
+            <h3>About</h3>
+            {this.props.project.description}
+          </main>
+
+          <aside className="rewards">
+          </aside>
+        </section>
+
       </div>
     );
   }
 }
 
 export default withRouter(ProjectShowPage);
-// <div>{this.props.project.title}</div>
+
+
+// <span>THE PROJECT SHOW PAGE</span>
+// {this.props.project.title}
+// {this.props.project.image_url}
+// {this.props.project.description}
+// {this.props.project.short_blurb}
+// {this.props.project.funding_amount}
+// {this.props.project.funding_end_date}
+// {this.props.project.category}
