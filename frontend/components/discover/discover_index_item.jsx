f1eb344c708eec4d6d00a8387ea0166bd433d201
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-02-09 04:00:49 -0800

    Styled discover results more closely to original site

--- frontend/components/discover/discover_index_item.jsx
+++ frontend/components/discover/discover_index_item.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { daysRemainingUntilEnd } from '../../utils/date_util';
 
 const DiscoverIndexItem = ({ project }) => {
   let linkHref = `/projects/${project.id}`;
   return (
     <div className="discover-item">
-      <Link to={linkHref}>
-        <img src={project.image_url}></img>
-      </Link>
-      <div className="discover-item-detail">
+      <div className="discover-item-top">
+        <Link to={linkHref}>
+          <img src={project.image_url}></img>
+        </Link>
+      </div>
+      <div className="discover-item-detail discover-item-middle">
         <Link to={linkHref}>
           <span className="project-title">{project.title}</span>
         </Link>
-        <div className="category-link">
-          <Link to="/discover">
-            {project.category}
-          </Link>
+      </div>
+      <div className="separating-bar"></div>
+      <div className="discover-item-bottom">
+        <span className="goal">
+          ${project.funding_amount} funding goal
+        </span>
+        <div>
+          <span className="days-to-go">
+            {daysRemainingUntilEnd(project.funding_end_date)}
+          </span>
+            days to go
         </div>
       </div>
     </div>
