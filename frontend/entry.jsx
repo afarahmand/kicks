8fa99affccc58de5799299a290155db0bfb1cb78
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-02-09 13:49:21 -0800

    Removed all functions set to window and the redux logger

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
-import { fetchSearchResults } from './utils/project_api_util';
-
-import {
-  fetchProjects,
-  fetchProject,
-  createProject,
-  updateProject,
-  deleteProject
-} from './actions/project_actions';
+// import { fetchSearchResults } from './utils/project_api_util';
+//
+// import {
+//   fetchProjects,
+//   fetchProject,
+//   createProject,
+//   updateProject,
+//   deleteProject
+// } from './actions/project_actions';
+//
+// import { formatAsYYYYMMDD } from './utils/date_util';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
@@ -38,25 +40,26 @@ document.addEventListener("DOMContentLoaded", () => {
     store = configureStore(preloadedState);
   }
 
-  window.fetchSearchResults = fetchSearchResults;
-  window.fetchProjects = fetchProjects;
-  window.fetchProject = fetchProject;
-  window.createProject = createProject;
-  window.updateProject = updateProject;
-  window.deleteProject = deleteProject;
-  window.firstProject = {
-    category: "Art",
-    description: "description",
-    funding_amount: 1,
-    funding_end_date: "2018-09-04T13:12:52.594Z",
-    id: 1,
-    image_url: "https://imgur.com/a/ZgBj0",
-    short_blurb: "blurb",
-    title: "Test",
-    user_id: 1
-  };
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  // window.formatAsYYYYMMDD = formatAsYYYYMMDD;
+  // window.fetchSearchResults = fetchSearchResults;
+  // window.fetchProjects = fetchProjects;
+  // window.fetchProject = fetchProject;
+  // window.createProject = createProject;
+  // window.updateProject = updateProject;
+  // window.deleteProject = deleteProject;
+  // window.firstProject = {
+  //   category: "Art",
+  //   description: "description",
+  //   funding_amount: 1,
+  //   funding_end_date: "2018-09-04T13:12:52.594Z",
+  //   id: 1,
+  //   image_url: "https://imgur.com/a/ZgBj0",
+  //   short_blurb: "blurb",
+  //   title: "Test",
+  //   user_id: 1
+  // };
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
 
   ReactDOM.render(<Root store={ store } />, root);
 });
