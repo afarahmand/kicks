7351794f2c5627a0d98594a37def6339667afc5c
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-03-15 13:21:25 -0700

    Refactored to remove extraneous code.  No functional changes

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -27,8 +27,3 @@ const usersReducer = (oldState = {}, action) => {
 };
 
 export default usersReducer;
-
-// export const RECEIVE_ALL_PROJECTS = "RECEIVE_ALL_PROJECTS";
-// export const RECEIVE_PROJECT = "RECEIVE_PROJECT";
-// export const REMOVE_PROJECT = "REMOVE_PROJECT";
-// export const RECEIVE_PROJECT_ERRORS = "RECEIVE_PROJECT_ERRORS";
