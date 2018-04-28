75453f50afdc70b0add35822df41d98d0533c37e
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-04-28 01:02:27 -0700

    Added rewards reducer and its associated error reducer to existing combine reducers

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import projectsErrorsReducer from './projects_errors_reducer';
+import rewardsErrorsReducer from './rewards_errors_reducer';
 import sessionErrorsReducer from './session_errors_reducer';
 
 const errorsReducer = combineReducers({
   projects: projectsErrorsReducer,
+  rewards: rewardsErrorsReducer,
   session: sessionErrorsReducer
 });
 
