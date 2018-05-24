7c6dfe71e6aa7fc9556836cd54079c220f468821
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-05-24 01:25:06 -0700

    Removed an unused action

--- frontend/reducers/rewards_errors_reducer.js
+++ frontend/reducers/rewards_errors_reducer.js
@@ -1,6 +1,5 @@
 import { merge } from 'lodash';
 import {
-  RECEIVE_ALL_PROJECT_REWARDS,
   RECEIVE_REWARD,
   RECEIVE_REWARD_ERRORS
 } from '../actions/reward_actions';
@@ -9,7 +8,6 @@ const rewardsErrorsReducer = (oldState = [], action) => {
   Object.freeze(oldState);
 
   switch(action.type) {
-    case RECEIVE_ALL_PROJECT_REWARDS:
     case RECEIVE_REWARD:
       return [];
     case RECEIVE_REWARD_ERRORS:
