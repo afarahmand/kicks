eab68ce30e641346cbb061b010d7e4036123b377
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-05-25 01:27:19 -0700

    Moved reward form components from 'reward' folder into a 'form' subfolder

---
+++ frontend/components/reward/form/form_item.jsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+
+class FormItem extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = props.reward;
+
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleSave = this.handleSave.bind(this);
+  }
+
+  handleDelete(e) {
+    e.preventDefault();
+    console.log("handleDelete: ", this.state);
+    console.log("this.props: ", this.props);
+    this.props.deleteReward(this.state);
+  }
+
+  handleSave(e) {
+    e.preventDefault();
+    let reward = Object.assign({}, this.state);
+    this.props.saveReward(reward);
+    // .then(
+    //   reward1 => {console.log("Success!!!")},
+    //   err => {console.log("Failure!!!")}
+    // );
+  }
+
+  hasAnyFieldBeenEdited() {
+    let stringifiedState = Object.assign({}, this.state);
+
+    if (typeof stringifiedState.amount === "string") {
+      stringifiedState.amount = Number(stringifiedState.amount);
+    }
+
+    return !_.isEqual(this.props.reward, stringifiedState);
+  }
+
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
+  renderSaveButton() {
+    if (this.hasAnyFieldBeenEdited()) {
+      return (
+        <button className="green">
+          Save
+        </button>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div className="reward-form">
+        <form onSubmit={this.handleSave}>
+          <div className="reward-top">
+            <table>
+              <tbody>
+
+                <tr>
+                  <th>Title</th>
+                  <td>
+                    <input
+                      type="text"
+                      value={this.state.title}
+                      onChange={this.update('title')}
+                    />
+                  </td>
+                </tr>
+
+                <tr>
+                  <th>Amount</th>
+                  <td>
+                    <input
+                      type="text"
+                      value={this.state.amount}
+                      onChange={this.update('amount')}
+                    />
+                  </td>
+                </tr>
+
+                <tr>
+                  <th>Description</th>
+                  <td>
+                    <textarea
+                      onChange={this.update('description')}
+                      className="description"
+                      value={this.state.description}
+                    >
+                    </textarea>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+          <div className="reward-bottom">
+            <button
+              onClick={this.handleDelete}
+              className="red"
+            >
+              Delete
+            </button>
+            {this.renderSaveButton()}
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default FormItem;
---
+++ frontend/components/reward/form/reward_form.jsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+
+import { formatAsYYYYMMDD } from '../../../utils/date_util';
+
+import FormItem from './form_item';
+
+class RewardForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+
+    this.addNewReward = this.addNewReward.bind(this);
+    this.removeReward = this.removeReward.bind(this);
+  }
+
+  componentDidMount() {
+    if (this.props.match.params.projectId) {
+      this.props.fetchProject(this.props.match.params.projectId);
+      // .then(
+      //   project => this.initState(project.rewards)
+      // );
+    }
+  }
+
+  addNewReward() {
+    // Generate new, unused rewardId
+    console.log("this.state: ", this.state);
+    console.log("this.state[i]: ", this.state[i]);
+    console.log("this.state[i] === undefined: ", this.state[i] === undefined);
+    let i = 0;
+    while (this.state[i] !== undefined) {
+      i++;
+    }
+
+    let newReward = {
+      id: i,
+      title: "",
+      amount: 0,
+      description: ""
+    };
+
+    this.setState({ id: newReward });
+  }
+
+  removeReward(id) {
+    console.log("removeReward: ", id);
+    let newState = Object.assign({}, this.state);
+    delete newState[id];
+
+    console.log("this.state: ", this.state)
+    this.setState({ newState }).then(
+      () => console.log(this.state)
+    );
+  }
+
+  renderErrors() {
+    return (
+      <div className="error-display">
+        <ul>
+          {this.props.errors.map((error, i) => (
+            <li key={`error-${i}`}> {error} </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="reward-form-page">
+          <h2>Set your rewards and shipping costs.</h2>
+          <section className="page-subtitle">
+            Invite backers to be a part of the creative experience by
+            offering rewards like a copy of what you’re making, a special
+            experience, or a behind-the-scenes look into your process.
+          </section>
+          <section className="reward-form-container">
+            {
+              Object.keys(this.props.projectRewards).map(rewardId => (
+                <FormItem
+                  key={rewardId}
+                  reward={this.props.projectRewards[rewardId]}
+                  deleteReward={this.props.deleteReward}
+                  saveReward={this.props.updateReward}
+                />
+              ))
+            }
+            {
+              Object.keys(this.state).map(rewardId => (
+                <FormItem
+                  key={rewardId}
+                  reward={this.state[rewardId]}
+                  deleteReward={this.removeReward}
+                  saveReward={this.props.createReward}
+                />
+              ))
+            }
+
+            <button
+              onClick={this.addNewReward}
+            >
+              Add New Reward +
+            </button>
+          </section>
+      </div>
+    );
+  }
+}
+
+export default withRouter(RewardForm);
