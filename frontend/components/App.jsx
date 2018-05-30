2f91af4c176611a9529883c3c79c9ccdf37b32e7
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-05-30 01:39:39 -0700

    Added frontend route to rewards form

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -6,8 +6,10 @@ import { AuthRoute, ProtectedRoute } from '../utils/route_util';
 import DiscoverContainer from './discover/discover_container';
 import HomePageContainer from './home/home_page_container';
 import NavbarContainer from './navbar/navbar_container';
+
 import ProjectFormContainer from './project/project_form_container';
 import ProjectShowPageContainer from './project/project_show_page_container';
+import RewardFormContainer from './reward/form/reward_form_container';
 import SearchPage from './search/search_page';
 import SessionFormContainer from './session/session_form_container';
 
@@ -20,16 +22,19 @@ const App = () => (
     <Switch>
       <AuthRoute exact path="/signin" component={SessionFormContainer} />
       <AuthRoute exact path="/signup" component={SessionFormContainer} />
-      <ProtectedRoute
-        exact
-        path="/projects/new"
-        component={ProjectFormContainer}
-      />
       <Route path="/discover" component={DiscoverContainer} />
       <Route path="/search" component={SearchPage} />
       <ProtectedRoute
+        path="/projects/new"
+        component={ProjectFormContainer}
+      />
+      <ProtectedRoute
         path="/projects/:projectId/edit"
         component={ProjectFormContainer}
+      />
+      <ProtectedRoute
+        path="/projects/:projectId/rewards/edit"
+        component={RewardFormContainer}
       />
       <Route
         exact
