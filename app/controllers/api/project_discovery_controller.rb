fb1a46ae4ee9a3614a3ac31500041bef4bff75a7
Ashil <ashil.farahmand@gmail.com>
2024-07-02 19:53:38 -0400

    Fix broken ActiveRecord query caused by change to new Rails version

--- app/controllers/api/project_discovery_controller.rb
+++ app/controllers/api/project_discovery_controller.rb
@@ -1,12 +1,12 @@
 class Api::ProjectDiscoveryController < ApplicationController
   def index
-    if (discovery_params[:category] == "")
+    if discovery_params[:category] == ""
       @projects = Project.all.limit(discovery_params[:numProjects])
     else
-      @projects = Project.discovery_results({
+      @projects = Project.discovery_results(
         category: discovery_params[:category],
         sort: discovery_params[:sort]
-      }).limit(discovery_params[:numProjects])
+      ).limit(discovery_params[:numProjects])
     end
 
     render "api/project_searches/index"
